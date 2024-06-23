c035dc43e0072668087e89c3f9c22a3d7cb262d5
sandipbibe9912 <sbibe01@gmail.com>
2024-06-23 20:38:40 +0530

    ""commit'

--- src/pages/Home/Leftsider.jsx
+++ src/pages/Home/Leftsider.jsx
@@ -9,7 +9,7 @@ const Leftsider = () => {
                    <a rel="noreferrer" href='mailto:sbibe01@gmail.com' target='_blank'>  <i class="ri-mail-unread-line text-gray-400 text-xl "></i> </a>
                    <a rel="noreferrer" href='https://www.instagram.com/its_sandy_ssr?igsh=cGFneWRudzNybThz' target='_blank'> <i class="ri-instagram-line text-gray-400 text-xl "></i> </a>
                    <a rel="noreferrer" href='https://www.linkedin.com/in/sandip-bibe-961a10211?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app' target='_blank'> <i class="ri-linkedin-fill text-gray-400 text-xl "></i> </a>
-                   {/* <a href='https://www.facebook.com/share/bYmZn5fHAuc3aMyz/?mibextid=qi2Omg' target='_blank'>  <i class="ri-github-fill text-gray-400 text-xl "></i> </a> */}
+                   
 
                 </div>
                 <div  className='w-[1px] block sm:none h-32 bg-[#1a5599]'></div>
